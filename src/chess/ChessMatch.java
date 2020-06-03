2e1923a481c3ca16578a7a129e437efc8371194c
FlavioSG-BR <flavio_godoy@yahoo.com.br>
2020-06-03 17:59:38 -0300

    Special move - Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,6 +4,7 @@ import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.security.InvalidParameterException;
 
 import boardgame.Board;
 import boardgame.Piece;
